f879c367cfdff0140a75c176154fa833a3a090ca
yhsh <1114701778@qq.com>
2020-07-07 13:53:25 +0800

    change some config

--- src/main/java/com/juxue/Application.java
+++ src/main/java/com/juxue/Application.java
@@ -10,16 +10,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableSwaggerBootstrapUI
-public class Application extends SpringBootServletInitializer {
-	/**
-	 * 开启web.xml 如果打包成war 放到tomcat 运行 必须 ，，如果打包成jar 则不需要
-	 * */
-	@Override
-	protected SpringApplicationBuilder configure(
-			SpringApplicationBuilder builder) {
-		// TODO Auto-generated method stub
-		return builder.sources(Application.class);
-	}
+public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
